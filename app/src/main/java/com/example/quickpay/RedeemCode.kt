5bd16d94dc8e739c93afada57d0615812b6bc8d0
oluwaseyi.ayodele <oluwaseyi.ayodele@arca.network>
2019-12-17 14:50:53 +0100

    QuickPay

---
+++ app/src/main/java/com/example/quickpay/RedeemCode.kt
@@ -0,0 +1,11 @@
+package com.example.quickpay
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RedeemCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redeem_code)
+    }
+}
